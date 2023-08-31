b80e75cc10c6291a9a7d6c8b98838f770ca67051
Abuzar Asif <107269011+Abuzarasif@users.noreply.github.com>
2023-09-01 01:06:45 +0500

    Add files via upload

---
+++ bulb on off/app.js
@@ -0,0 +1,9 @@
+function bulbon() {
+    let bulb = document.getElementById("bulb");
+    bulb.src = "images/on.png";
+}
+
+function bulboff() {
+    let bulb = document.getElementById("bulb");
+    bulb.src = "images/off.png";
+}
