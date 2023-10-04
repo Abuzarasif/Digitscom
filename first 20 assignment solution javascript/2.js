b7264eef55436875602d0984fdde6b3704c0e40d
Abuzar Asif <107269011+Abuzarasif@users.noreply.github.com>
2023-10-05 04:12:29 +0500

    Add files via upload

---
+++ first 20 assignment solution javascript/2.js
@@ -0,0 +1,25 @@
+// declare variable
+let username;
+// declare message variable
+let myname = 'abuzar asif ';
+// titled message 
+let title_message;
+title_message = "hellow world ";
+alert(title_message);
+// showing data in alert 
+let firstname = "jhone dhone";
+let age = " 15 years old";
+let certifications = " certified Mobile application development ";
+
+alert(firstname + " " + age + " " + certifications);
+// pizza message
+let pizza = "pizza \n pizz \n piz \n pi \n p";
+alert(pizza);
+// email
+let email = "abuzarasif19@gmail.com";
+alert("my email is " + " " + email);
+// book
+let book = "A smarter \n way to learn javascript";
+alert("i am trying to learn from the book  " + book);
+// document.write
+document.write(" yah i can write html through  js ");
