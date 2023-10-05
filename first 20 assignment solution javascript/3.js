8664a95d77f7ec904abccce24380006a6dcb4ebb
Abuzar Asif <107269011+Abuzarasif@users.noreply.github.com>
2023-10-06 02:31:58 +0500

    Add files via upload

---
+++ first 20 assignment solution javascript/3.js
@@ -0,0 +1,15 @@
+// 1
+let age = 27;
+alert("i am" + age + "years old ");
+// 2
+let N = 14;
+alert("you have visited this website" + N + "times ");
+// 3
+let dateofbirth = 2000;
+document.write("my birth year is " + dateofbirth + "\n");
+document.write("data type of my declared variable is number ");
+// 4
+let visitor_name = "abubakar";
+let produt_title = "motorcycle";
+let quantity = 25;
+document.write(visitor_name + "ordered" + quantity + produt_title + " on XYZ store");
