63a4bf242756c6a2a58653ec6bbc808d0ad7a9c0
Abuzar Asif <107269011+Abuzarasif@users.noreply.github.com>
2023-10-07 17:53:52 +0500

    Add files via upload

---
+++ first 20 assignment solution javascript/4.js
@@ -0,0 +1,175 @@
+// q1
+// 1 adding 2 numbers
+
+// let a = prompt("please enter the first number ");
+// let b = prompt("please enter the first number ");
+// let sum = parseFloat(a) + parseFloat(b);
+// document.write("the sum of " + a + " and " + b + " is " + sum);
+// q2
+// multiplication
+
+
+// let c = prompt("please enter the first number ");
+// let d = prompt("please enter the first number ");
+// let mul = parseFloat(c) * parseFloat(d);
+// document.write("the multiplication  of " + c + " and " + d + " is " + mul);
+
+
+// subtraction
+
+// let e = prompt("please enter the first number ");
+// let f = prompt("please enter the first number ");
+// let sub = parseFloat(e) - parseFloat(f);
+// document.write("the multiplication  of " + e + " and " + f + " is " + sub);
+
+// division
+
+// let g = prompt("please enter the first number ");
+// let h = prompt("please enter the first number ");
+// let div = parseFloat(g) / parseFloat(h);
+// document.write("the multiplication  of " + g + " and " + h + " is " + div);
+
+// modulus
+
+// let i = prompt("please enter the first number ");
+// let j = prompt("please enter the first number ");
+// let mod = parseFloat(i) % parseFloat(j);
+// document.write("the multiplication  of " + i + " and " + j + " is " + mod);
+
+// q3
+
+// let numb;
+// alert("value after declaration of variable is" + numb);
+// numb = 27;
+// alert(" initialize value is " + numb);
+// numb++;
+// alert("value after increment is " + numb);
+// numb = numb + 7;
+// alert("value after addition is " + numb);
+// numb--;
+// alert("value after decrement is " + numb);
+// let remainder = numb % 3;
+// alert("the remainder is " + remainder);
+
+// Q4
+
+// let movie_ticket = 600;
+// let no_tickets = prompt(" enter the number of 5220tickets you want to buy ");
+// parseInt(no_tickets);
+// let price_ticket = 600 * no_tickets;
+// document.write("total cost to buy " + no_tickets + " tickets to a movie is" + price_ticket + "PKR");
+
+// Q5
+
+// let table = prompt("please enter the number you want to find the table")
+// parseInt(table);
+// for (let m = 1; m <= 10; m++) {
+//     let table_num = table * m;
+//     document.write("table of" + table + "*" + m + "is" + table_num);
+// }
+
+// q6
+
+// let a = prompt("please enter the temperature in farenhiet  ");
+// document.write("converting it into celcius ");
+// let c = (a - 32) * 5 / 9;
+// let b = prompt("please enter the temperature in celcius ");
+// document.write("converting it into farenhiet");
+// let d = (b * 9 / 5) + 32;
+// document.write("the tempertaure in celcius is " + c);
+// document.write("the tempertaure in farenhiet is " + d);
+
+
+// let a = prompt("please enter the temperature  ");
+// let c = (a - 32) * 5 / 9;
+// let d = (a * 9 / 5) + 32;
+// document.write("the tempertaure in celcius is " + c);
+// document.write("the tempertaure in farenhiet is " + d);
+
+// q7
+// let price1 = prompt("please enter a price of first item");
+// parseFloat(price1);
+// let price2 = prompt("please enter a price of second item");
+// parseFloat(price2);
+// let order1 = prompt("please enter a price of first order");
+// parseFloat(order1);
+// let order2 = prompt("please enter a price of second order");
+// parseFloat(order2);
+// let shipping = prompt("please enter a price of shipping charges of item");
+// parseFloat(shipping);
+
+// not applicable let total_cost = price1 + price2 + order1 + order2 + shipping;
+
+// let total_cost = parseInt(price1) + parseInt(price2) + parseInt(order1) + parseInt(order2) + parseInt(shipping);
+
+// not applicable parseFloat(total_cost);
+
+// document.write("price of item 1 is" + price1);
+// document.write("quantity of item 1 is " + order1);
+// document.write("price of item 2 is" + price2);
+// document.write("quantity of item 2 is" + order2);
+// document.write("Shipping charges is" + shipping);
+
+// document.write("Total cost of your order is " + total_cost);
+
+// q8
+
+// let total_marks = 100;
+// let Obtained_marks = 89;
+
+// let percent = Obtained_marks / total_marks * 100;
+
+// document.write("total marks" + total_marks);
+// document.write("obtained marks" + Obtained_marks);
+// document.write(" percentage" + percent);
+
+// q9
+
+// let dolar = 10;
+// let riyal = 25;
+// let one_dolar = 104.85;
+// let one_riyal = 28;
+// let totaldolar_pkr = dolar * one_dolar;
+// let totalriyal_pkr = riyal * one_riyal;
+// let totalcurrency = totaldolar_pkr + totalriyal_pkr;
+// document.write("Total currency in pkr: " + totalcurrency);
+
+
+// q10
+
+// let num = 27;
+// let sum = 27 + 5 * 10 / 2;
+// document.write("all equation equals to " + sum);
+
+
+// q11
+
+// let current_year = 2023;
+// let dateofbirth = prompt("please enter your birth year");
+// let age = current_year - dateofbirth;
+// document.write("your age is " + age);
+
+// q12
+
+// let radius = 20;
+// let circumference = 2 * 3.142 * radius;
+// let area = 3.142 * radius * radius;
+// document.write("radius: " + radius);
+// document.write("circumference: " + circumference);
+// document.write("Area: " + area);
+
+// q13
+let fav_snack = ("lays");
+let age = 15;
+let max_age = 65;
+let estimated_amount = 3;
+let years_left = max_age - age;
+let days_left = years_left * 365;
+total_snackseaten = years_left * days_left;
+document.write("fav snacks=" + fav_snack);
+document.write("age=" + age);
+document.write("max age=" + max_age);
+document.write("estimated amount=" + estimated_amount);
+document.write("years left=" + years_left);
+document.write("days left=" + days_left);
+document.write("total snacks=" + total_snackseaten);
