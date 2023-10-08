1557f679a38a1fa2c0f25121f31a485516bf8c62
Abuzar Asif <107269011+Abuzarasif@users.noreply.github.com>
2023-10-08 19:54:16 +0500

    Add files via upload

---
+++ first 20 assignment solution javascript/5.js
@@ -0,0 +1,81 @@
+// q1
+
+// let num = prompt("please enter a number ");
+// parseInt(num);
+// document.write("the value of num is :" + num);
+// num++;
+// document.write("the value of num++ is :" + num);
+// document.write("Now the value of num is :" + num);
+
+// document.write("Now the value of num++ is :" + num);
+// num++;
+// document.write("the value of num is :" + num);
+// num--;
+// document.write("the value of num is :" + num);
+// document.write("the value of num is :" + num);
+// num--;
+// document.write("the value of num is :" + num);
+
+// q2
+
+// let a = 2;
+// let b = 1;
+
+
+// --a;
+// document.write(a);
+
+// let result = --a - --b + ++b + b--;
+
+// --a;
+// --a - --b;
+// --a - --b + ++b;
+
+// --a - --b + ++b + b--;
+
+
+// document.write(result)
+
+// q3
+
+// let fname = prompt("please enter your name ");
+// document.write("hellow mr:" + fname);
+
+
+// q4
+
+
+
+// let num = prompt("please enter the number ");
+// let tab;
+// if (num != "") {
+//     for (let i = 1; i <= 10; i++) {
+//         tab = num * i;
+//         document.write(tab);
+//     }
+// }
+// if (num == "") {
+
+//     for (let j = 1; j <= 10; j++) {
+//         tab = 5 * j;
+//         document.write(tab);
+//     }
+
+// }
+
+// q5
+
+// let sub_one = prompt("please enter the first subject");
+// let sub_two = prompt("please enter the two subject");
+// let sub_three = prompt("please enter the three subject");
+
+// let total_marks = 100;
+
+// let num_one = prompt("please enter the marks for the first subject");
+// let num_two = prompt("please enter the marks for the second subject");
+// let num_three = prompt("please enter the marks for the third  subject");
+
+// let total = num_one + num_two + num_three;
+// let percentage = total / 300 * 100;
+// document.write("total: " + total);
+// document.write("percentage: " + percentage);
